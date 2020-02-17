30ba8be4d2dc73ed3759aa9eb27551255741689f
Exul <idelacruz.alberto@gmail.com>
2020-02-17 00:02:19 -0300

    Se agrego midifaciones en js

--- Javascript/index.js
+++ Javascript/index.js
@@ -2,3 +2,6 @@ let numeros = [1,8,4,9,34,67,109]
 console.log(numeros);
 let arrayMezclado = [99,"Iron man",4.77 ,true,false]
 console.log(arrayMezclado);
+let miArray = [];
+miArray.push("Deadpool");
+console.log(miArray);
